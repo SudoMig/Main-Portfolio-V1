6801326791546585fdabeb956aeacb7f0fdc4074
sudomig <mu_@tuta.io>
2022-08-17 15:53:16 -0400

    Workflow pIcture updated

--- src/components/Data/WorkFlowData.jsx
+++ src/components/Data/WorkFlowData.jsx
@@ -1,17 +1,21 @@
 import Design from '../../Images/Design.png'
+import WorkFlowClient from '../../Images/WorkFlowClient.png'
+import WorkFlowWireFrame from '../../Images/WorkFlowWireFrame.png'
+import WorkFlowDevelopment from '../../Images/WorkFlowDevelopment.png'
+import WorkFlowHandOff from '../../Images/WorkFlowHandOff.png'
 
 
 
 const WorkflowData = [
   {
     num: '01',
-    src: Design,
+    src: WorkFlowClient,
     title: 'Client Approach',
     description: ' Pren kalilig inte os bitest. Dåteras pongen mit, bol. Hang kisk antekinat. Antesk infraplan sasamma dipinar. Biodårar besöse.Astronera vavöheten dun.',
   },
   {
     num: '02',
-    src: Design,
+    src: WorkFlowWireFrame,
     title: 'WireFrame',
     description: ' Pren kalilig inte os bitest. Dåteras pongen mit, bol. Hang kisk antekinat. Antesk infraplan sasamma dipinar. Biodårar besöse.Astronera vavöheten dun.',
   },
@@ -23,13 +27,13 @@ const WorkflowData = [
   },
   {
     num: '04',
-    src: Design,
+    src: WorkFlowDevelopment,
     title: 'Developemnt',
     description: ' Pren kalilig inte os bitest. Dåteras pongen mit, bol. Hang kisk antekinat. Antesk infraplan sasamma dipinar. Biodårar besöse.Astronera vavöheten dun.',
   },
   {
     num: '05',
-    src: Design,
+    src: WorkFlowHandOff,
     title: 'Project HandOff',
     description: ' Pren kalilig inte os bitest. Dåteras pongen mit, bol. Hang kisk antekinat. Antesk infraplan sasamma dipinar. Biodårar besöse.Astronera vavöheten dun.',
   },
--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -6,35 +6,7 @@ import WorkflowData from './Data/WorkFlowData';
 const Workflow = () => {
   return (
     <div className=''>
-      <div className='radiantWorkFlowMobile w-full block lg:hidden'>
-        <Container>
-          <div>
-            <div className='flex justify-left text-white pt-24 pl-4 md:pt-24 md:pl-8 md:mb-14 sm:pt-48 sm:pl-16 sm:mb-24'>
-              <div className='text-lg sm:text-xl'>
-                WorkFlow
-                <div className='grid grid-rows-2'>
-                  <div className='box-content w-80 text-left font-semibold text-2xl sm:text-3xl pl-10 sm:pl-12'>
-                    Overview
-                  </div>
-                  <div className='bg-PrimaryBlue h-1 w-48 ml-10 ml-12'></div>
-                </div>
-              </div>
-            </div>
-          </div>
-          {WorkflowData.map((item, index) => (
-            <WorkFlowItems
-              key={index}
-              index={index}
-              num={item.num}
-              src={item.src}
-              title={item.title}
-              description={item.description}
-            />
-          ))}
-        </Container>
-      </div>
-
-      <div className='radiantWorkFlow w-full lg:block hidden'>
+      <div className='radiantWorkFlow w-full'>
         <Container>
           <div>
             <div className='flex justify-left text-white pt-24 pl-4 mb-20 sm:pt-48 sm:pl-16 sm:mb-24'>
