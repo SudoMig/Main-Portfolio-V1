4c51809d64ee3e3e802aba72be5c5ee7b6da4ebf
sudomig <mu_@tuta.io>
2022-08-26 10:05:28 -0400

    Updated BG header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import video from '../Videos/video.mp4';
+import bannerImage from '../Images/bannerImage.png'
 
 function Header() {
   return (
@@ -9,14 +10,16 @@ function Header() {
           <h1 className='relative z-30 font-semibold text-5xl sm:text-7xl text-white tracking-superWide'>
             Welcome
           </h1>
-          <video
+          <img className='absolute z-10 w-auto min-w-full max-w-none' src={bannerImage} alt="" />
+          {/* VIDEO OPTION */}
+          {/* <video
             loop
             autoPlay
             className='absolute z-10 w-auto min-w-full max-w-none'
           >
             <source src={video} type='video/mp4' />
             Your browser does not support the video tag.
-          </video>
+          </video> */}
           <div className='z-40 absolute left-0 md:left-5 md:bottom-0 lg:bottom-10 lg:left-16 text-white'>
             <p className='sm:text-lg'>Web Designer & Developer</p>
             <p className='box-content h-12 w-64 font-semibold md:text-2xl sm:text-3xl sm:pt-2 sm:pb-14'>
