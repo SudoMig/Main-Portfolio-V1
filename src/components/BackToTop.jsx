aa1b329db91b4f68160512a14e369921aedcb144
sudomig <mu_@tuta.io>
2022-08-29 16:11:19 -0400

    fixed SBTT button bg

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -26,7 +26,7 @@ const BackToTop = () => {
       <>
     
           {showButton && (
-          <button onClick={scrollToTop} className="z-50 fixed  md:right-12 bottom-12 border-2 border-blue-600 bg-transparent w-10 h-16 bg-white text-xl text-slate-100 font-semibold flex justify-center items-center text-center rounded-full hover:bg-blue-600 transition duration-300 ease-out">
+          <button onClick={scrollToTop} className="z-50 fixed md:right-12 md:bottom-12 border-2 border-blue-600 bg-transparent w-10 h-16 text-xl text-slate-100 font-semibold flex justify-center items-center text-center rounded-full hover:bg-blue-600 transition duration-300 ease-out">
             <FaAngleDoubleUp/>
           </button>
         )}
