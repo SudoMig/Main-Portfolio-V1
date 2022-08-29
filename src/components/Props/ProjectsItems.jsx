d2c76719a09ddc57938285879bbdad9c20a2ccf7
sudomig <mu_@tuta.io>
2022-08-29 11:25:52 -0400

    button colour changed to blue-800

--- src/components/Props/ProjectsItems.jsx
+++ src/components/Props/ProjectsItems.jsx
@@ -6,15 +6,26 @@ const ProjectsItems = (props) => {
       <div className='rounded-lg w-full'>
         <div className='w-full transition-all duration-700 ease-in-out'>
           <img
-        //   brightness-75 hover:grayscale-0 hover:brightness-100
+            data-aos='fade-right'
+            data-aos-duration='700'
+            //   brightness-75 hover:grayscale-0 hover:brightness-100
             className='hover:shadow-lg hover:shadow-blue-500/30 hover:scale-125 rounded-lg border-t border-gray-800 transition-all duration-700 ease-in-out  '
             src={props.src}
             alt=''
           />
-          <div className='py-14 px-4'>
-            <div className='text-white text-xl font-semibold mb-2'>{props.title}</div>
+          <div
+            data-aos='fade-right'
+            data-aos-duration='700'
+            data-aos-delay="500"
+            className='py-14 px-4'
+          >
+            <div className='text-white text-xl font-semibold mb-2'>
+              {props.title}
+            </div>
             <div className='text-white'>{props.content}</div>
-            <button className='bg-slate-100 text-black h-10 w-28 mt-4 font-semibold cursor-default hvr-bounce-to-right'>More Soon</button>
+            <button className='bg-slate-100 text-black h-10 w-28 mt-4 font-semibold cursor-default hvr-bounce-to-right'>
+              More Soon
+            </button>
           </div>
         </div>
       </div>
