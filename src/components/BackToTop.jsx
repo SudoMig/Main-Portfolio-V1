da922cc22be966b3838a855a336d4af62ec42456
sudomig <mu_@tuta.io>
2022-08-31 09:39:36 -0400

    changed hover:bg SBTP

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -26,7 +26,7 @@ const BackToTop = () => {
       <>
     
           {showButton && (
-          <button onClick={scrollToTop} className="z-50 fixed bottom-10 right-6 md:right-12 md:bottom-12 w-10 h-10 md:w-10 md:h-16 border-2 border-blue-100 bg-transparent  text-xl text-blue-100 font-semibold flex justify-center items-center text-center rounded-full hover:bg-blue-600 transition duration-300 ease-out">
+          <button onClick={scrollToTop} className="z-50 fixed bottom-10 right-6 md:right-12 md:bottom-12 w-10 h-10 md:w-10 md:h-16 border-2 border-blue-100 bg-transparent text-xl text-blue-100 font-semibold flex justify-center items-center text-center rounded-full hover:bg-blue-700 transition duration-300 ease-out">
             <FaAngleDoubleUp/>
           </button>
         )}
