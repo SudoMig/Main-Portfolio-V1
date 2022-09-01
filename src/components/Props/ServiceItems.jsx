3333e9870331134a82ab7ed852ae7ae09fb58f90
sudomig <mu_@tuta.io>
2022-09-01 16:21:09 -0400

    changed invisible to opacity services & added text to dev

--- src/components/Props/ServiceItems.jsx
+++ src/components/Props/ServiceItems.jsx
@@ -2,15 +2,15 @@ const ServiceItems = (props) => {
   return (
     <div className='max-w-8xl mx-auto'>
       <div className='flex flex-nowrap w-full md:h-[38rem] lg:h-[42rem]'>
-        <div className='group flex-auto md:w-10 lg:w-24 bg-MobileWebsiteDesign bg-cover bg-center transition-all duration-700 hover:grow-[2]'>
-          <div className='hover:backdrop-blur-sm h-full'>
+        <div className='group flex-auto md:w-10 lg:w-24 bg-MobileWebsiteDesign bg-cover bg-center transition-all duration-700 hover:grow-[2] z-0'>
+          <div className='lg:hover:backdrop-blur-sm lg:h-full'>
             <div className='grid grid-cols-2 pt-6'>
               <div className='col-end-1 md:h-24 lg:-28 w-1 bg-blue-600 rounded-full ml-6 mr-4'></div>
               <div className='box-content lg:w-56 font-semibold text-2xl text-white mt-2'>
                 Website Design
               </div>
             </div>
-            <div className='box-content md:w-64 lg:w-[29rem] text-blue-100 ml-10 invisible group-hover:visible transition-opacity-100 duration-200 ease-out '>
+            <div className='box-content md:w-64 lg:w-[29rem] text-blue-50 ml-10 opacity-0 group-hover:opacity-100 transition-opacity-100 duration-200 ease-out '>
               <div className='mb-4'>
                 The website design is the first thing any user sees on your
                 website. It can, unfortunately serve as a deterrent or,
@@ -30,43 +30,43 @@ const ServiceItems = (props) => {
           </div>
         </div>
 
-        <div className='group flex-auto md:w-10 lg:w-24 bg-WebsiteDevelopment bg-cover bg-center transition-all duration-700 hover:grow-[2]'>
-          <div className='hover:backdrop-blur-sm h-full'>
+        <div className='group flex-auto md:w-10 lg:w-24 bg-WebsiteDevelopment bg-cover bg-center transition-all duration-700 hover:grow-[2] z-10'>
+          <div className='lg:hover:backdrop-blur-sm lg:h-full'>
             <div className='grid grid-cols-2 pt-6'>
               <div className='col-end-1 md:h-24 lg:-28 w-1 bg-blue-600 rounded-full ml-6 mr-4'></div>
               <div className='box-content lg:w-56 font-semibold text-2xl text-white mt-2'>
                 Website Development
               </div>
             </div>
-            <div className='box-content md:w-64 lg:w-[29rem] text-blue-100 ml-10 invisible group-hover:visible transition-opacity-100 duration-300 ease-out '>
+            <div className='box-content md:w-64 lg:w-[29rem] text-blue-50 ml-10 opacity-0 group-hover:opacity-100 transition-opacity-100 duration-300 ease-out'>
               <div className='mb-4'>
-                The website design is the first thing any user sees on your
-                website. It can, unfortunately serve as a deterrent or,
-                fortunately, an incentive to browse your website. It has the
-                possibility of serving several purposes, tailored to the
-                client's needs and requirements. Ultimately, it is meant to
-                improve the user experience and target a particular audience. A
-                quality design goes a long way toward establishing trust and
-                professionalism for your company.
+                I develop websites utilising the popular JavaScript Framework
+                ReactJS. When developing websites, I ensure to keep clean and
+                organised code to reduce bugs and minimise future complications.
+                Animations are growing in popularity. This may come at the cost
+                of the website's speed. In this instance, it&apos;s vital to
+                undertake the respective approach, including unidirectional
+                components, to ensure speed optimization. In ReactJS, a
+                functional component approach is recommended.
               </div>
               <div>
-                Every respectable website needs a well designed website. Whether
-                you solely need a design template or a website, the option is
-                yours to choose from.
+                Whether you already have a design available or need one, I will
+                gladly develop a quality and affordable website in an
+                appropriate language.
               </div>
             </div>
           </div>
         </div>
 
-        <div className='group flex-auto md:w-10 lg:w-24 bg-PersonalizedRequests bg-cover bg-center transition-all duration-700 hover:grow-[2]'>
-          <div className='hover:backdrop-blur-sm h-full'>
+        <div className='group flex-auto md:w-10 lg:w-24 bg-PersonalizedRequests bg-cover bg-center transition-all duration-700 hover:grow-[2] z-20'>
+          <div className='lg:hover:backdrop-blur-sm lg:h-full'>
             <div className='grid grid-cols-2 pt-6'>
               <div className='col-end-1 md:h-24 lg:-28 w-1 bg-blue-600 rounded-full ml-6 mr-4'></div>
               <div className='box-content lg:w-56 font-semibold text-2xl text-white mt-2'>
                 Personal Request
               </div>
             </div>
-            <div className='box-content md:w-64 lg:w-[29rem] text-blue-100 ml-10 invisible group-hover:visible transition-opacity-100 duration-300 ease-out '>
+            <div className='box-content md:w-64 lg:w-[29rem] text-blue-50 ml-10 opacity-0 group-hover:opacity-100 transition-opacity-100 duration-300 ease-out '>
               <div className='mb-4'>
                 The website design is the first thing any user sees on your
                 website. It can, unfortunately serve as a deterrent or,
@@ -86,15 +86,15 @@ const ServiceItems = (props) => {
           </div>
         </div>
 
-        <div className='group flex-auto md:w-10 lg:w-24 bg-Others bg-cover bg-center transition-all duration-700 hover:grow-[2]'>
-          <div className='hover:backdrop-blur-sm h-full'>
+        <div className='group flex-auto md:w-10 lg:w-24 bg-Others bg-cover bg-center transition-all duration-700 hover:grow-[2] z-30'>
+          <div className='lg:hover:backdrop-blur-sm lg:h-full'>
             <div className='grid grid-cols-2 pt-6'>
               <div className='col-end-1 md:h-24 lg:-28 w-1 bg-blue-600 rounded-full ml-6 mr-4'></div>
               <div className='box-content lg:w-56 font-semibold text-2xl text-white mt-2'>
                 Bugs & Fixes
               </div>
             </div>
-            <div className='box-content md:w-64 lg:w-[29rem] text-blue-100 ml-10 invisible group-hover:visible transition-opacity-100 duration-300 ease-out '>
+            <div className='box-content md:w-64 lg:w-[29rem] text-blue-50 ml-10 opacity-0 group-hover:opacity-100 transition-opacity-100 duration-300 ease-out '>
               <div className='mb-4'>
                 The website design is the first thing any user sees on your
                 website. It can, unfortunately serve as a deterrent or,
