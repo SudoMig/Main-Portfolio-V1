887c78d1c18378c778bda00d0d198ff7c04d6c58
sudomig <mu_@tuta.io>
2022-09-01 17:15:16 -0400

    added service text to service data

--- src/components/Data/ServicesData.jsx
+++ src/components/Data/ServicesData.jsx
@@ -2,30 +2,30 @@ const ServicesData = [
   {
     title: 'Website Design',
     paragraph:
-      'Makropura doplara. Pode dedade homonas, huruvida trev. Retresamma kosona i fin nuligt. Prende pyse suling föledes.',
+      "The website design is the first thing any user sees on your website. It can, unfortunately serve as a deterrent or, fortunately, an incentive to browse your website. It has the possibility of serving several purposes, tailored to the client's needs and requirements. Ultimately, it is meant to improve the user experience and target a particular audience. A quality design goes a long way toward establishing trust and professionalism for your company.",
     paragraph2:
-      'Makropura doplara. Pode dedade homonas, huruvida trev. Retresamma kosona i fin nuligt. Prende pyse suling föledes.',
+      " Every respectable website needs a well designed website. Whether you solely need a design template or a website, the option is yours to choose from.",
   },
   {
     title: 'Website Development',
     paragraph:
-      'Makropura doplara. Pode dedade homonas, huruvida trev. Retresamma kosona i fin nuligt. Prende pyse suling föledes. ',
+      "I develop websites utilising the popular JavaScript Framework ReactJS. When developing websites, I ensure to keep clean and organised code to reduce bugs and minimise future complications. Animations are growing in popularity. This may come at the cost of the website's speed. In this instance, it's vital to undertake the respective approach, including unidirectional components to ensure speed optimization. In ReactJS, a functional component approach is recommended.",
     paragraph2:
-      'Makropura doplara. Pode dedade homonas, huruvida trev. Retresamma kosona i fin nuligt. Prende pyse suling föledes.',
+      'Whether you already have a design available or need one, I will gladly develop a quality and affordable website.',
   },
   {
     title: 'Special Request',
     paragraph:
-      'Makropura doplara. Pode dedade homonas, huruvida trev. Retresamma kosona i fin nuligt. Prende pyse suling föledes. ',
+      "I understand that clients may have unique needs and requirements that aren't generally offered. I am happy to accept unique website and design related requests that are within my skill set.",
     paragraph2:
-      'Makropura doplara. Pode dedade homonas, huruvida trev. Retresamma kosona i fin nuligt. Prende pyse suling föledes.',
+      'Please do not hesitate to contact me via the contact section below for any questions or requests.',
   },
   {
     title: 'Bugs and Fixes',
     paragraph:
-      'Makropura doplara. Pode dedade homonas, huruvida trev. Retresamma kosona i fin nuligt. Prende pyse suling föledes.',
+      " Have bugs been bugging your website for a while? Let me know via the contact section below with a brief and detailed description.",
     paragraph2:
-      'Makropura doplara. Pode dedade homonas, huruvida trev. Retresamma kosona i fin nuligt. Prende pyse suling föledes.',
+      "If it's within my area of expertise, I will gladly take a look and fix it for the experience.",
   },
 ];
 
