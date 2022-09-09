d71aeae8504789f7a59fa7ade007d7a40ee068fd
sudomig <mu_@tuta.io>
2022-09-09 19:00:50 -0400

    remove extra div

--- src/components/Props/AccordionItem.jsx
+++ src/components/Props/AccordionItem.jsx
@@ -21,51 +21,47 @@ const AccordionItem = (props) => {
 
   return (
     <div>
-      <div>
-        <div>
-          <Container>
-            <div className='flex flex-col justify-center'>
-              <div className='relative mb-4'>
-                <div className='absolute left-[-2rem] top-4 h-28 w-[0.4rem] rounded-lg'>
-                  <div
-                    className={` absolute w-[0.4rem] h-0 bg-blue-100 z-0
-                ${
-                  active === props.index
-                    ? '!bg-blue-600 !h-full h-1 rounded-lg transform duration-700 ease-out z-10 '
-                    : '!h-10 w-[0.4rem] rounded-lg z-10 '
-                }`}
-                  ></div>
-                </div>
-              </div>
-              {/*  Progress Overview Content  */}
-
-              <div className='font-bold text-lg text-white mb-4 z-10'>
-                <button
-                  onClick={(e) => eventHandler(e, props.index)}
-                  className={
-                    active === props.index ? Style.Activate : Style.Inactive
-                  }
-                  aria-expanded={active === props.index ? 'true' : 'false'}
-                  aria-controls={'select-' + indexCount(props.index)}
-                  aria-disabled={active === props.index ? 'true' : 'false'}
-                >
-                  <span>{props.title}</span>
-                </button>
-              </div>
-              <div className=''>
-                <div
-                  id={'select-' + indexCount(props.index)}
-                  className={
-                    active === props.index ? Style.PanelOpen : Style.PanelClose
-                  }
-                >
-                  {props.description}
-                </div>
-              </div>
+      <Container>
+        <div className='flex flex-col justify-center'>
+          <div className='relative mb-4'>
+            <div className='absolute left-[-2rem] top-4 h-28 w-[0.4rem] rounded-lg'>
+              <div
+                className={` absolute w-[0.4rem] h-0 bg-blue-100 z-0
+            ${
+              active === props.index
+                ? '!bg-blue-600 !h-full h-1 rounded-lg transform duration-700 ease-out z-10 '
+                : '!h-10 w-[0.4rem] rounded-lg z-10 '
+            }`}
+              ></div>
             </div>
-          </Container>
+          </div>
+          {/*  Progress Overview Content  */}
+
+          <div className='font-bold text-lg text-white mb-4 z-10'>
+            <button
+              onClick={(e) => eventHandler(e, props.index)}
+              className={
+                active === props.index ? Style.Activate : Style.Inactive
+              }
+              aria-expanded={active === props.index ? 'true' : 'false'}
+              aria-controls={'select-' + indexCount(props.index)}
+              aria-disabled={active === props.index ? 'true' : 'false'}
+            >
+              <span>{props.title}</span>
+            </button>
+          </div>
+          <div className=''>
+            <div
+              id={'select-' + indexCount(props.index)}
+              className={
+                active === props.index ? Style.PanelOpen : Style.PanelClose
+              }
+            >
+              {props.description}
+            </div>
+          </div>
         </div>
-      </div>
+      </Container>
     </div>
   );
 };
