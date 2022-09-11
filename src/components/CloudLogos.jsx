e8e663687109e96444c7a1a5eea5c4a3f94517ad
sudomig <mu_@tuta.io>
2022-09-11 15:29:50 -0400

    pt-16 Clodlogo

--- src/components/CloudLogos.jsx
+++ src/components/CloudLogos.jsx
@@ -8,7 +8,7 @@ import Container from '../sections/Container';
 
 const CloudLogos = () => {
   return (
-    <div className='bg-black pt-12 pb-36 md:pt-28 md:pb-36 lg:pt-4   '>
+    <div className='bg-black pt-16 pb-36 md:pt-28 md:pb-36 lg:pt-4   '>
       <Container>
         <h1
           className='text-xl text-center text-white font-semibold capitalize pb-4 md:pb-6 sm:pb-12'
