420c3f9cfcb88aa5bff9c04e5dd320da71b8172f
sudomig <mu_@tuta.io>
2022-09-27 17:37:28 -0400

    changed colour of side bar services &  mobile

--- src/components/Props/ServiceItems.jsx
+++ src/components/Props/ServiceItems.jsx
@@ -5,7 +5,7 @@ const ServiceItems = (props) => {
         <div className='group flex-auto md:w-10 lg:w-24 bg-MobileWebsiteDesign bg-cover bg-center transition-all duration-700 hover:grow-[2] z-0'>
           <div className='lg:hover:backdrop-blur-sm lg:h-full'>
             <div className='grid grid-cols-2 pt-6'>
-              <div className='col-end-1 md:h-24 lg:-28 w-1 bg-blue-600 rounded-full ml-6 mr-4'></div>
+              <div className='col-end-1 md:h-24 lg:-28 w-1 bg-PaleGreen rounded-full ml-6 mr-4'></div>
               <div className='box-content lg:w-56 font-semibold text-2xl text-white mt-2'>
                 Website Design
               </div>
@@ -32,7 +32,7 @@ const ServiceItems = (props) => {
         <div className='group flex-auto md:w-10 lg:w-24 bg-WebsiteDevelopment bg-cover bg-center transition-all duration-700 hover:grow-[2] z-10'>
           <div className='lg:hover:backdrop-blur-sm lg:h-full'>
             <div className='grid grid-cols-2 pt-6'>
-              <div className='col-end-1 md:h-24 lg:-28 w-1 bg-blue-600 rounded-full ml-6 mr-4'></div>
+              <div className='col-end-1 md:h-24 lg:-28 w-1 bg-PaleGreen rounded-full ml-6 mr-4'></div>
               <div className='box-content lg:w-56 font-semibold text-2xl text-white mt-2'>
                 Website Development
               </div>
@@ -58,7 +58,7 @@ const ServiceItems = (props) => {
         <div className='group flex-auto md:w-10 lg:w-24 bg-PersonalizedRequests bg-cover bg-center transition-all duration-700 hover:grow-[2] z-20'>
           <div className='lg:hover:backdrop-blur-sm lg:h-full'>
             <div className='grid grid-cols-2 pt-6'>
-              <div className='col-end-1 md:h-24 lg:-28 w-1 bg-blue-600 rounded-full ml-6 mr-4'></div>
+              <div className='col-end-1 md:h-24 lg:-28 w-1 bg-PaleGreen rounded-full ml-6 mr-4'></div>
               <div className='box-content lg:w-56 font-semibold text-2xl text-white mt-2'>
                 Personal Request
               </div>
@@ -81,7 +81,7 @@ const ServiceItems = (props) => {
         <div className='group flex-auto md:w-10 lg:w-24 bg-Others bg-cover bg-center transition-all duration-700 hover:grow-[2] z-30'>
           <div className='lg:hover:backdrop-blur-sm lg:h-full'>
             <div className='grid grid-cols-2 pt-6'>
-              <div className='col-end-1 md:h-24 lg:-28 w-1 bg-blue-600 rounded-full ml-6 mr-4'></div>
+              <div className='col-end-1 md:h-24 lg:-28 w-1 bg-PaleGreen rounded-full ml-6 mr-4'></div>
               <div className='box-content lg:w-56 font-semibold text-2xl text-white mt-2'>
                 Bugs & Fixes
               </div>
